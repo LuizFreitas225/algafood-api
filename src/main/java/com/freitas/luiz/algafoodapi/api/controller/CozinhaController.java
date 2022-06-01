a48093190b3eda739082ec257ee2ee6725b79887
Luiz Nicolau <luiz.freitas@ilia.digital>
2022-06-01 15:27:37 -0300

    deletar cozinha

--- src/main/java/com/freitas/luiz/algafoodapi/api/controller/CozinhaController.java
+++ src/main/java/com/freitas/luiz/algafoodapi/api/controller/CozinhaController.java
@@ -2,9 +2,11 @@ package com.freitas.luiz.algafoodapi.api.controller;
 
 import java.util.List;
 
+import com.fasterxml.jackson.databind.util.BeanUtil;
 import com.freitas.luiz.algafoodapi.domain.model.Cozinha;
 import com.freitas.luiz.algafoodapi.domain.repository.CozinhaRepository;
 import com.freitas.luiz.algafoodapi.model.CozinhasWrapper;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -38,6 +40,36 @@ public class CozinhaController {
 			return ResponseEntity.ok(cozinha);
 		}
 
+		return  ResponseEntity.notFound().build();
+	}
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cozinha salvar(@RequestBody  Cozinha cozinha) {
+		return  cozinhaRepository.salvar(cozinha);
+	}
+
+	@PutMapping("/{id}")
+	@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<Cozinha> atualizar(@RequestBody  Cozinha cozinha,@PathVariable Long id) {
+		Cozinha cozinhaAtual = cozinhaRepository.buscar(id);
+        if(cozinhaAtual != null){
+			BeanUtils.copyProperties(cozinha,cozinhaAtual, "id");
+
+			cozinhaAtual = cozinhaRepository.salvar(cozinhaAtual);
+			return ResponseEntity.ok(cozinhaAtual) ;
+		}
+		return  ResponseEntity.notFound().build();
+	}
+
+	@DeleteMapping ("/{id}")
+	@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<Cozinha> deletar(@RequestBody  Cozinha cozinha,@PathVariable Long id) {
+		Cozinha cozinhaAtual = cozinhaRepository.buscar(id);
+		if(cozinhaAtual != null){
+			cozinhaRepository.remover(cozinhaAtual);
+			return  ResponseEntity.noContent().build();
+		}
 		return  ResponseEntity.notFound().build();
 	}
 }
